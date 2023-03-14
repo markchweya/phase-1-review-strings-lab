b0c1b49e8a4a20c310bede56a3e24faecb1c0f1c
Mark <mark.nyambane@student.moringaschool.com>
2023-03-14 21:49:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+const currentUser = 'Mark Chweya';
+
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!';
+
+const shortGreeting = 'Welcome ${currentUser[0]}!'
+
